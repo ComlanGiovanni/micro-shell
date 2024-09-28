b0574ed8ed03bc0c2ec1516887867fd934835815
Giovanni Comlan <gicomlan@student.42.fr>
2024-09-28 13:25:50 +0200

    Merge from private 42-Exam-Rank-04 git && setup program
    Missing: Makefile Norm macor for commande fai etc wip

---
+++ includes/ft_microshell_macros.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_microshell_macros.h                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gicomlan <gicomlan@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/28 12:47:46 by gicomlan          #+#    #+#             */
+/*   Updated: 2024/09/28 13:22:23 by gicomlan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_MICRO_SHELL_MACROS_H
+# define FT_MICRO_SHELL_MACROS_H
+
+#define PIPE "|"
+#define SEMICOLON ";"
+#define CHANGE_DIRECTORY "cd"
+
+"error: ", "cd: bad arguments"
+"error: cd: cannot change directory to "
+
+
+#endif //!FT_MICRO_SHELL_MACROS_H
